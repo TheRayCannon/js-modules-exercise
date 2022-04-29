7cf191cfff8390d7c21ff77654869b7f35c6af1a
Shawn Cannon <scanno16@ford.com>
2022-04-29 14:52:01 -0600

    complete last steps

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const arrayOne = [1, 2, 3]
 const arrayTwo = [2, 3, 4]
+
+import findDifferences from "./difference"
+
+findDifferences(arrayOne, arrayTwo)
+const p = document.querySelector("Result")
+p.append(findDifferences(arrayOne, arrayTwo))
